861265ba1c3d9aadb18e308cbeb2519f7895c806
oyegoke-chima <106771353+Oyegoke-chima@users.noreply.github.com>
2026-03-30 18:40:42 +0100

    fix AOS init using useEffect in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 import Home from './pages/Home'
 import Give from './pages/Give'
 import Prayer from './pages/Prayer'
 import About from './pages/About'
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      easing: 'ease-in-out',
+      once: true,
+    })
+  }, [])
+
   return (
     <BrowserRouter>
       <Routes>
